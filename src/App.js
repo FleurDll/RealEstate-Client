36b13f3c936066d041ad084388cc0cd8ac39efcb
Fleur <fleur.dalle@hotmail.com>
2021-05-10 15:47:58 +0200

    add _redirects file in public folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, HashRouter, Switch, Route } from "react-router-dom";
 import Footer from "./components/footer";
 import Home from "./pages/Home";
 import SingleBiens from "./pages/singleBiens";
